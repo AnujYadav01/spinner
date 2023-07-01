8802a205d13f2ca6db0f647bc8946c3d3ded8b3d
Anuj Yadav <anuj9kumar.ak@gmail.com>
2023-07-01 23:37:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    name: "circle",
+  };
+
+  startRotation = () => {
+    this.setState({
+      name: "circle start-rotate",
+    });
+    setTimeout(() => {
+      this.setState({
+        name: "circle start-rotate stop-rotate",
+      });
+    }, Math.floor(Math.random() * 10000) + 1);
+  };
+  render() {
+    return (
+      <div>
+        <div className="arrow"></div>
+        <ul className={this.state.name}>
+          <li>
+            <div className="text" contentEditable="true" spellCheck="false">
+              $100
+            </div>
+          </li>
+          <li>
+            <div className="text" contentEditable="true" spellCheck="false">
+              $200
+            </div>
+          </li>
+          <li>
+            <div className="text" contentEditable="true" spellCheck="false">
+              $300
+            </div>
+          </li>
+          <li>
+            <div className="text" contentEditable="true" spellCheck="false">
+              $400
+            </div>
+          </li>
+          <li>
+            <div className="text" contentEditable="true" spellCheck="false">
+              $500
+            </div>
+          </li>
+          <li>
+            <div className="text" contentEditable="true" spellCheck="false">
+              $600
+            </div>
+          </li>
+          <li>
+            <div className="text" contentEditable="true" spellCheck="false">
+              Try Again
+            </div>
+          </li>
+          <li>
+            <div className="text" contentEditable="true" spellCheck="false">
+              $800
+            </div>
+          </li>
+          <li>
+            <div className="text" contentEditable="true" spellCheck="false">
+              $900
+            </div>
+          </li>
+          <li>
+            <div className="text" contentEditable="true" spellCheck="false">
+              $1000
+            </div>
+          </li>
+          <li>
+            <div className="text" contentEditable="true" spellCheck="false">
+              Try Again
+            </div>
+          </li>
+          <li>
+            <div className="text" contentEditable="true" spellCheck="false">
+              $1200
+            </div>
+          </li>
+        </ul>
+        <button className="spin-button" onClick={this.startRotation}>
+          SPIN
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
